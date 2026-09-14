d81b1391d84aa5408ef6656b872e7724965028ba
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-14 19:41:49 +0530

    3rd

--- Test.js
+++ Test.js
@@ -8,5 +8,13 @@
 
 // Declaare and Initialize the variable
 
+// a = 10
+// console.log(a+10)
+
+// a = "Rs"
+// console.log(a)
+
 a = 10
-console.log(a+10)
+console.log(a)
+a = 10.23
+console.log(a)
