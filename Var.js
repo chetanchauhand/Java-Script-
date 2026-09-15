5a1e4d7f2fef76f1ceb00feda5663adf845fa80d
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-15 14:23:34 +0530

    1st

---
+++ Var.js
@@ -0,0 +1,12 @@
+let accountId = 12345
+let accountEmail = "ChetanChauhan@gmail.com"
+var accountPassaword = "102030"
+let accountCity = "Punjab"
+
+accountId = 2
+accountEmail = "Raj@gmail.com"
+accountPassaword = "2536"
+accountCity = "Delhi"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassaword,accountCity]);
