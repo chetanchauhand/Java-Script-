e0a4269364a47908ceaede02b1428345f05d5e83
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-16 19:18:05 +0530

    1st

--- Test.js
+++ Test.js
@@ -1,3 +1,6 @@
+// Basic Coding
+
+
 // console.log("Chetan")
 // console.log(3)
 // console.log("3")
@@ -14,7 +17,23 @@
 // a = "Rs"
 // console.log(a)
 
-a = 10
+// a = 10
+// console.log(a)
+// a = 10.23
+// console.log(a)
+// b = 10
+// console.log(b)
+// b++
+// console.log(b)
+
+// a = 1
+// b = 3
+// c = 4
+// console.log(a,b,c)
+// console.log("Raj" , "Is" ,"Good" , "Boy")
+// a = 21
+// console.log("My age is :"+a)
+var a = 2
 console.log(a)
-a = 10.23
+var a = 3
 console.log(a)
