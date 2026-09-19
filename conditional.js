456cc9ca64900a93eeb544a6e7e54520559f193c
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-19 18:58:10 +0530

    2nd

--- conditional.js
+++ conditional.js
@@ -1,6 +1,12 @@
 // Condititional statement
 
-age = 22
-if (age=>18) console.log("Adult")
+// age = 22
+// if (age=>18) console.log("Adult")
 
-else console.log("Teen")
+// else console.log("Teen")
+
+// If my number is event.
+
+num = 8
+if(num%2==0) console.log("Even")
+else("Odd")
