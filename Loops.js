dff0fb5ec410f95fa8f4fb50bec3f770185a9edc
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-19 19:33:13 +0530

    8th

--- Loops.js
+++ Loops.js
@@ -1,12 +1,22 @@
 //  For Loof
 
 // for(i = 0;i<=10;i++) {
-for(let i=0;i<=10;i++){
-    console.log(i)
-}
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// while Loop
+
+// i = 0
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+// Do while Loop
 
 i = 0
-while(i<=10){
+do{
     console.log(i)
     i++
-}
+   }while(i<=10)
