d3259802aaf0a04a18d48f73f4c9efb6e4bc0d99
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-19 19:16:08 +0530

    4th

--- cond.js
+++ cond.js
@@ -5,8 +5,14 @@ rating = 5
 // 4 = Good
 // 5 = Exellent
 
-if(rating == 1) console.log("Poor")
-else if(rating == 2) console.log("Below Average")
-else if(rating == 3) console.log("Average")
-else if(rating == 4) console.log(" Good ")
-else console.log("Exellent ")
+// if(rating == 1) console.log("Poor")
+// else if(rating == 2) console.log("Below Average")
+// else if(rating == 3) console.log("Average")
+// else if(rating == 4) console.log(" Good ")
+// else console.log("Exellent ")
+
+num = 900
+
+if(num>=100 && num<= 990)  console.log("Num is  a three digit")
+
+else console.log("Num is not 3 digit")
