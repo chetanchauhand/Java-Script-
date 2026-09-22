924b2d3a17efa30b961b828a37a82cc61a625951
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-22 14:00:39 +0530

    8th

--- carr.js
+++ carr.js
@@ -3,5 +3,8 @@
 // arr[0]=1
 // console.log(arr)
 
-arr = [1,2,3,4,"Hello",true]
+arr = [1,2,3,4,"Hello",true] // we can put any type of datatype in array
+console.log(arr)
+
+arr = [[1,2,3,4],[5,6,7,8]] // 2D array
 console.log(arr)
