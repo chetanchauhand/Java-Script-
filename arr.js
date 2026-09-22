51e56822376db27a1c691aac73b0ab1b129bec31
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-22 13:40:03 +0530

    5th

--- arr.js
+++ arr.js
@@ -1,6 +1,11 @@
 arr = [1,2,3,4,5]
 console.log(arr)
-n = arr.length
-console.log(n)
-console.log(arr[0])
-console.log(arr[3])
+// n = arr.length
+// console.log(n)
+// console.log(arr[0])
+// console.log(arr[3])
+
+// push element in array
+
+arr.push(8)
+console.log(arr)
