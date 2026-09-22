a1034a9ff34d986f8df1e3c4e72f271f6d2d229d
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-22 18:39:10 +0530

    1st

--- forarr.js
+++ forarr.js
@@ -1,7 +1,15 @@
 // For loop in array
 
-arr = [1,2,3,"Hello",true,4,5,6,[6,7,8]]
-for(let i =1;i<=arr.length;i++){
-    console.log(i,arr[i])
+// arr = [1,2,3,"Hello",true,4,5,6,[6,7,8]]
+// for(let i =1;i<=arr.length;i++){
+//     console.log(i,arr[i])
 
+// }
+
+// Forof loop in javascript
+
+arr = [12,13,14,15]
+for (const element of arr) {
+    console.log(arr)
+    
 }
