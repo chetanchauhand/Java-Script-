a19f1d4e822d6d642e05743ffbd7d79513e88f85
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 20:58:03 +0530

    6th

--- obj.js
+++ obj.js
@@ -3,8 +3,11 @@ var details = ["Ram",22,92.5,false];
 
 let x = {
     name : "Ram",
+    'nick name':"Prabhu",
     age : 22,
+    'School Age':21,
     marks : 92.5,
     isMarried : false
 }
 console.log(x)
+
