d75b88bbd17ef4a7e8a34fc7f157756555885d49
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 21:37:14 +0530

    1st

--- MethodString.js
+++ MethodString.js
@@ -1,16 +1,16 @@
-// let s = "Hello Hi"
-// console.log(s,s.length)
-// let s2 = s.trim()
-// console.log(s2,s2.length)
-// console.log(s2.indexOf['e'])
-// console.log(s2.slice())
+let s = "Hello Hi"
+console.log(s,s.length)
+let s1 = s.trim()
+console.log(s1,s1.length)
+console.log(s1.indexOf['e'])
+console.log(s1.slice())
 
-// let s = "HalloEveryone"
-// console.log(s.slice(5))
-// console.log(s.slice(2,5)) //Substring 2 to 5
+let s2 = "HalloEveryone"
+console.log(s2.slice(5))
+console.log(s2.slice(2,5)) //Substring 2 to 5
 
-let s = "Hello everyone how are you and what are you doing"
-console.log(s)
+let s3 = "Hello everyone how are you and what are you doing"
+console.log(s3)
 // arr = s.split()
 let arr = s.split(' ')
 console.log(arr)
---
+++ call.js
@@ -0,0 +1,5 @@
+//CallBack function in javaScript
+function pro(a,b,c){
+    return a*b*c
+}
+console.log(pro(12,1.45,3.45))
