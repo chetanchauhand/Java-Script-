bde497827ae103eae25f157cb0ba1f427f9e7ae3
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 19:12:06 +0530

    2nd

--- truth.js
+++ truth.js
@@ -1,6 +1,12 @@
 // Truth or False value print
+// False valye is -> null,0,NAN,undefined
+// truth value is -> Everything accept truthy value
 
-if(1.5){
+
+if(NaN){
+// if(0){
+// if(null){
+// if(undefined){
     console.log("hello")
 }
 else console.log("Mello")
