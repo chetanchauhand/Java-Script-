f9c45e9dd1f2f569fcc87adcafd862278d7acbdc
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 21:15:47 +0530

    10th

---
+++ Math.js
@@ -0,0 +1,9 @@
+// Maths trick
+
+console.log(Math.abs(1,2,3,12,1.23))
+console.log(Math.max(12,13,4,4224,121,-123))
+console.log(Math.min(12,313,2114,34431,-1))
+console.log(Math.pow(12,131,414,491,-123))
+console.log(Math.log(12,124,535,654))
+console.log(Math.sqrt(12,124,535,654))
+console.log(Math.cbrt(12,124,535,654))
