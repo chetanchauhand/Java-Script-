56df5c8b93cd24b778130416026c6a78487ac604
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 19:36:42 +0530

    5th

--- MethodString.js
+++ MethodString.js
@@ -5,6 +5,12 @@
 // console.log(s2.indexOf['e'])
 // console.log(s2.slice())
 
-let s = "HalloEveryone"
-console.log(s.slice(5))
-console.log(s.slice(2,5)) //Substring 2 to 5
+// let s = "HalloEveryone"
+// console.log(s.slice(5))
+// console.log(s.slice(2,5)) //Substring 2 to 5
+
+let s = "Hello everyone how are you and what are you doing"
+console.log(s)
+// arr = s.split()
+let arr = s.split(' ')
+console.log(arr)
