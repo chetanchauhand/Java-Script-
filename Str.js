be3e78c45e6b78a980c55cc7272513238abb6114
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 19:17:55 +0530

    2nd

--- Str.js
+++ Str.js
@@ -1,5 +1,10 @@
 let s = "Chetan Chauhan"
 console.log(s,s.length)
-for(i=1;i<=s.length;i++){
-    console.log(s[i])
+// for(i=1;i<=s.length;i++){
+//     console.log(s[i])
+// }
+
+for (const ele of s) {
+    console.log(ele)
+    
 }
