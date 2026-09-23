e723a8b8ac14e5877f00c1ecd80321ef5d5bd538
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 20:50:47 +0530

    ini

--- Tamplate.js
+++ Tamplate.js
@@ -2,3 +2,6 @@
 let age = 30
 let exp = 10
 console.log(`My age is ${age} and my experiance is ${exp}`)
+
+console.log("Hello\"Hi")
+console.log(`Hello"Hi`)
---
+++ obj.js
@@ -0,0 +1,10 @@
+// Object in js
+var details = ["Ram",22,92.5,false];
+
+let x = {
+    name : "Ram",
+    age : 22,
+    marks : 92.5,
+    isMarried : false
+}
+console.log(x)
