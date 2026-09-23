c97d011752450152306f813f8a888cc955cc3cc2
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-23 19:32:14 +0530

    4th

--- MethodString.js
+++ MethodString.js
@@ -1,6 +1,10 @@
-let s = "Hello Hi"
-console.log(s,s.length)
-let s2 = s.trim()
-console.log(s2,s2.length)
-console.log(s2.indexOf['e'])
-console.log(s2.slice())
+// let s = "Hello Hi"
+// console.log(s,s.length)
+// let s2 = s.trim()
+// console.log(s2,s2.length)
+// console.log(s2.indexOf['e'])
+// console.log(s2.slice())
+
+let s = "HalloEveryone"
+console.log(s.slice(5))
+console.log(s.slice(2,5)) //Substring 2 to 5
