1d05c1841b7c6ef5c7a9906454df21874b77242a
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-24 19:05:58 +0530

    1st

--- BasicsOfDoms/script.js
+++ BasicsOfDoms/script.js
@@ -1,2 +1,14 @@
-let x = document.querySelectorAll(".Yes") // multiple users purpose
-console.log(x)
+// let x = document.querySelectorAll(".Yes") // multiple users purpose
+// console.log(x)
+
+let x = document.querySelector("h1")
+x.style.color = "Black"
+setTimeout(function(){
+    x.innerHTML = "Changed"
+},4000)
+
+let y = document.querySelector("h2")
+y.style.background = "red"
+setTimeout(function(){
+    y.innerHTML = "Changed"
+},2000)
