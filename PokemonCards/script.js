bc9e34e97f5a621a68ceade18f4ee489552d5526
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-25 19:05:19 +0530

    2nd

--- PokemonCards/script.js
+++ PokemonCards/script.js
@@ -5,11 +5,12 @@ let arr =
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLRSLIjgC0YarhjZJg5pcCSi_okN1eKP-ofO8WEAihpg&s",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtHiGLuHovkyeU6w0pqNQMQ_4ypiixu0No7ZQWhUp8ng&s=10",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBz2ADVTP-f0JHA54x_9IbTtFpFn-VRQIMfU20z0WjPg&s=10",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw2n_c3V8XIA35S3WF1hoCtzi_bVZhGUo9nPCgtOENBw&s=10",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrUZeKCUo4xsX25vVHE95rs1LNHZg_wLjnY0N7dP-qEw&s=10"]
 
 let s = "";
 for(let i=0;i<=55;i++){
-    let r = Math.floor(Math.random()*6)
+    let r = Math.floor(Math.random()*7)
     s+= `<div class="card"><img src=${arr[r]}></div>`;
 }
 
